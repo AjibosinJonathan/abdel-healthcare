b39a162364e3b70d7d9808217e433caedc8e6110
Jonathan Ajibosin <jonathanajibosin30@gmail.com>
2026-06-07 18:38:15 +0100

    Proper fix for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/contact", element: <h1>Contact Page</h1> },
-]);
+], {
+  basename: "/abdel-healthcare"
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
